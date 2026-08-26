d8acbc6de86e2d97ac0cb6f4407990d971c1b260
29miaoetlrsdnet <29miaoet@lrsd.net>
2026-08-26 18:32:43 -0500

    Updated files

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,24 @@
+return {
+    "catgoose/nvim-colorizer.lua",
+    ft = {
+        "html",
+        "css",
+        "scss",
+        "sass",
+        "javascript",
+        "typescript",
+        "jsx",
+        "tsx",
+    },
+    opts = {
+        options = {
+            parsers = {
+                css = true,
+            },
+            display = {
+                mode = "background",
+            },
+        },
+    },
+}
+
---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    config = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_auto_close = 1
+    end,
+  },
+}
+
